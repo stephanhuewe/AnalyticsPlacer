5f2864fb8ec676149719eba6c62cd5653255359f
stephanhuewe <sh@huewe.info>
2015-06-22 19:22:50 +0200

    Initial Checkin

---
+++ Huestel.Tools.AnalyticsPlacer.App/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Huestel.Tools.AnalyticsPlacer.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 362);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(580, 42);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(251, 47);
+            this.btnReplace.TabIndex = 1;
+            this.btnReplace.Text = "Place Analytics Code in Files";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 459);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnReplace;
+    }
+}
+
---
+++ Huestel.Tools.AnalyticsPlacer.App/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Huestel.Tools.AnalyticsPlacer.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string rootDirectoryPath = "";
+            string GACode = "";
+
+
+            List<FileInfo> sourceFiles = GetFilesList(rootDirectoryPath);
+            List<FileInfo> replacedFiles = PlaceAnalyticsCodeInFiles(GACode, sourceFiles);
+        }
+
+        private List<FileInfo> PlaceAnalyticsCodeInFiles(string gaCode, List<FileInfo> sourceFiles)
+        {
+            List<FileInfo> ret = new List<FileInfo>();
+
+            foreach (var file in sourceFiles)
+            {
+                string fileContent = File.ReadAllText(file.FullName);
+
+                string resultString = null;
+                try
+                {
+                    resultString = Regex.Match(fileContent, "</head>", RegexOptions.IgnoreCase | RegexOptions.Singleline).Value;
+                }
+                catch (ArgumentException ex)
+                {
+                    // Syntax error in the regular expression
+                }
+
+
+                if (!String.IsNullOrWhiteSpace(resultString))
+                {
+
+                    fileContent = fileContent.Replace(resultString, gaCode + resultString);
+                    File.WriteAllText(file.FullName, fileContent);
+                    ret.Add(file);
+                }
+            }
+
+            return ret;
+        }
+
+        private List<FileInfo> GetFilesList(string rootDirectoryPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
